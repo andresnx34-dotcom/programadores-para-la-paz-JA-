34c83063e7d67b35f792b4065c3cf2c69bef5238
andresnx34 <andresnx34@gmail.com>
2026-06-16 11:20:40 -0500

    actividad semana 6

--- semana6/server.js
+++ semana6/server.js
@@ -5,8 +5,8 @@ app.use(express.json());
 
 app.post('/registro', (req, res) => {
 
-  const nombre = req.body.nombre;
-  const mensaje = req.body.mensaje;
+  const nombre = req.body.nombre; "maria"
+  const mensaje = req.body.mensaje; "hola comunidad"
 
   res.json({
     estado: "Datos recibidos",
@@ -16,6 +16,15 @@ app.post('/registro', (req, res) => {
 
 });
 
-app.listen(3000, () => {
-  console.log('Servidor ejecutándose en puerto 3000');
+app.post('/incidencia', (req, res) => {
+
+  const tipo = req.body.tipo;
+  const descripcion = req.body.descripcion;
+
+  res.json({
+    mensaje: "Incidencia registrada",
+    tipo: tipo,
+    descripcion: descripcion
+  });
+
 });
